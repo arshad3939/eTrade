4ccdced6874134d7da9239adff9c613a7b582442
Arshad qureshi <arshadqureshi3939@gmail.com>
2023-02-17 23:23:22 +0530

    update browserRouter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './pages/Footer';
 const App = () => {
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header/>
       <Routes>
         <Route path='/' element={<Home />}></Route>
